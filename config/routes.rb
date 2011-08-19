794462341bb176f0d5d940b7616e120fce5439b6
Gera Barnabas <quward@gmail.com>
2011-08-19 19:41:37 +0200

    destroy

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ ActionController::Routing::Routes.draw do |map|
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
+	map.edit	'/edit',   :controller => 'users', :action => 'edit'
 
   map.signup '/signup',   :controller => 'users', :action => 'new'
 
